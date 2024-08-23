f770a34407a2da06b0f13c4b48190d076f1c6996
Yasin Walum <ywalum@gmail.com>
2024-08-23 11:32:14 +0300

    header bg color change

--- components/main.tsx
+++ components/main.tsx
@@ -4,7 +4,7 @@ import JobList from "./JobList";
 export default function Main() {
   return (
     <>
-      <header className="min-h-[150px] bg-border bg-[url('/images/bg-header-mobile.svg')] md:bg-[url('/images/bg-header-desktop.svg')] bg-no-repeat bg-center bg-cover" />
+      <header className="min-h-[150px] bg-primary bg-[url('/images/bg-header-mobile.svg')] md:bg-[url('/images/bg-header-desktop.svg')] bg-no-repeat bg-center bg-cover" />
       <main className="container w-full">
         <FilteredCards />
         <JobList />
